efd99a3e47ef580ad97c9eae7bada153a4e9c4d7
hunwei <627626110@qq.com>
2024-09-03 05:03:38 +0800

    Update component.tsx

--- src/components/component.tsx
+++ src/components/component.tsx
@@ -173,7 +173,7 @@ export function Component() {
       // 这里可以安全地使用 localStorage
       localStorage.setItem('key', 'value');
     }
-     if (ls) return JSON.parse(ls);
+    
     else return [];
   }, []);
 
